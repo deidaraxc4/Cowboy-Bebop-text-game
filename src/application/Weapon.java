98824cd24c2aeb36085be9e8aa977de91dbc509f
ColsonIRL <colsoncraig1@gmail.com>
2017-04-18 15:36:05 -0400

    Add files via upload

--- src/application/Weapon.java
+++ src/application/Weapon.java
@@ -1,5 +1,22 @@
-package application;
+package src.application;
 
-public class Weapon {
+import java.util.*;
+import java.io.*;
+public class Weapon
+{
+    private String description;
+    private int minDamage;
+    private int maxDamage;
+    private static ArrayList<Weapon> x = new ArrayList<Weapon>();
 
+    /**
+     * Constructor for objects of class Weapon
+     */
+    public Weapon(String description, int minDamage, int maxDamage)
+    {
+        this.description = description;
+        this.minDamage = minDamage;
+        this.maxDamage = maxDamage;
+    }
+   
 }
